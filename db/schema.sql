21e02c608e9aaa300d25bfcd90c5981d5067e85c
Yuki Yamanaka <yuukiyamanaka13@gmail.com>
2019-07-10 01:36:48 +0900

    change schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `db`  DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `db`;
 
-DROP TABLE IF EXISTS `bargain_info`;
-CREATE TABLE `bargain_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_id` int(11) DEFAULT NULL,
-  `shop_id` int(11) DEFAULT NULL,
-  `price` int(11) unsigned DEFAULT NULL,
-  `product_name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `product_id_idx` (`product_id`),
-  KEY `shop_id_idx` (`shop_id`),
-  CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `product_info` (`product_id`),
-  CONSTRAINT `shop_id` FOREIGN KEY (`shop_id`) REFERENCES `shop_info` (`id`)
-);
-
---
--- Table structure for table `product_info`
---
-
 DROP TABLE IF EXISTS `product_info`;
 CREATE TABLE `product_info` (
   `product_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -41,3 +22,22 @@ CREATE TABLE `shop_info` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 );
+
+DROP TABLE IF EXISTS `bargain_info`;
+CREATE TABLE `bargain_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) DEFAULT NULL,
+  `shop_id` int(11) DEFAULT NULL,
+  `price` int(11) unsigned DEFAULT NULL,
+  `product_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `product_id_idx` (`product_id`),
+  KEY `shop_id_idx` (`shop_id`),
+  CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `product_info` (`product_id`),
+  CONSTRAINT `shop_id` FOREIGN KEY (`shop_id`) REFERENCES `shop_info` (`id`)
+);
+
+--
+-- Table structure for table `product_info`
+--
