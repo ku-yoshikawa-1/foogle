54e167194c1f6953fa593bf68331b5d36a1007f0
Yuki Yamanaka <yuukiyamanaka13@gmail.com>
2019-07-25 01:27:54 +0900

    change filename

--- front-end/src/store/store.js
+++ front-end/src/store/store.js
@@ -1,11 +1,11 @@
 import Vuex from 'vuex'
 import general_state from './general_state'
-import maps from './maps'
+import search from './search'
 
 
 export default new Vuex.Store({
   modules: {
     general_state,
-    maps,
+    search,
   }
 })
